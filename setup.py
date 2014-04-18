eecdb596221c416676270ba40c39b635992e1a6a
David Danier <david.danier@team23.de>
2014-04-18 18:24:01 +0200

    Fixed packaging

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "django_hits",
-    version = "0.2.0-1",
+    version = "0.2.0-2",
     description = 'Count visitors/views for your page',
     author = 'David Danier',
     author_email = 'david.danier@team23.de',
@@ -12,6 +12,8 @@ setup(
         'django_hits',
         'django_hits.migrations',
         'django_hits.templatetags',
+        'django_hits.management',
+        'django_hits.management.commands',
     ],
     install_requires = [
         'Django >=1.3',
